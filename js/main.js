d8c09d2154e6b153b5d2b9ee419c77c7e9a8532d
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-08-20 15:12:33 -0400

    finished light switch assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // let body = document.querySelector('body');
+    // body.style.backgroundColor = 'black';
+    
+    let status = document.querySelector('.status');
+    status.innerText = "The page has loaded!";
 
+    let body = document.querySelector('body');
+    let button = document.querySelector('.switch');
 
+    button.addEventListener('click', function () {
+        let buttonClass = button.className;
+        if (buttonClass == ('switch on'))  {
+            button.className = 'switch off';
+            body.className = 'dark';
+            status.innerText = "Hey, who turned off the lights?";
+        } else if (buttonClass == ('switch off')) {
+            button.className = 'switch on';
+            body.className = 'light';
+            status.innerText = "It's so bright in here!";
+        }
+    });
+});
